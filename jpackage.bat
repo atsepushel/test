fed5acfcff321e35ed48304af20a858f0f25d31a
Andrii <Andrii@MACHINE-IQOB8V8>
2020-09-07 18:14:21 +0300

    Fixes

--- jpackage.bat
+++ jpackage.bat
@@ -1,1 +1,10 @@
-"C:\Program Files\Java\jdk-14.0.2\bin\jpackage.exe" --module-path "C:\Program Files\Java\jdk-14.0.2\jmods" --verbose --runtime-image "C:\Program Files\Java\jdk-14.0.2" --input "J:\test\lib" --dest "J:\test\finalBuild" --name test --main-jar main_app.jar --main-class com.mycompany.testswing.TestFrame --type exe
+"C:\Program Files\Java\jdk-14.0.2\bin\jpackage.exe" ^
+--type exe ^
+--verbose ^
+--module-path "C:\Program Files\Java\jdk-14.0.2\jmods" ^
+--runtime-image "C:\Program Files\Java\jdk-14.0.2" ^
+--input "J:\test\lib" ^
+--dest "J:\test\finalBuild" ^
+--name test ^
+--main-jar main_app.jar ^
+--main-class com.mycompany.testswing.TestFrame 
